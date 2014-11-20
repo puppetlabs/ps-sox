08a895b6ec6fc8c4c5d6eef5a4fcd4b187c62357
Zack Smith <zack@puppetlabs.com>
2014-11-20 11:14:36 -0800

    Fix Path

--- manifests/keyserv.pp
+++ manifests/keyserv.pp
@@ -1,10 +1,10 @@
-# Manage gdm.conf
-class sox::gdm(
+# Manage keyserv.conf
+class sox::keyserv(
   $fixit = false,
 ) {
-  if $::check_gdm == 'Failed' and $fixit {
-    ini_setting { 'gdm.conf AllowRoot=false:':
-      path    => '/etc/X11/gdm/gdm.conf',
+  if $::check_keyserv == 'Failed' and $fixit {
+    ini_setting { 'keyserv.conf AllowRoot=false:':
+      path    => '/etc/X11/keyserv/keyserv.conf',
       setting => 'AllowRoot',
       section => '',
       value   => 'false',
