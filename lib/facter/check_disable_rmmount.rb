946c7857e83c02acfc5cb0f4f2e2e50c165d665d
Zack Smith <zack@puppetlabs.com>
2014-11-21 13:50:48 -0800

    Add disable rmount check

---
+++ lib/facter/check_disable_rmmount.rb
@@ -0,0 +1,24 @@
+Facter.add(:check_disable_rmmount) do
+  confine :kernel => 'Linux'
+  setcode do
+    if File.exist? '/etc/auto.misc'
+      # Assume failure if not matched below
+      result = 'Passed'
+      File.open('/etc/auto.misc','r') do |file|
+        file.each_line do |line|
+          # Skip any comments in the file
+          next if line =~ /^#.*$/
+          # If the line is matched break out and pass
+          if line =~ /.*\/dev\/cdrom.*$/
+            result = 'Failed'
+            break
+          end
+        end
+      end
+      result
+    else
+      # If file does not exist we pass the test
+      'Passed'
+    end
+  end
+end
