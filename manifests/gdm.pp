f9473b5b01955b530de7915938c1df323218c8fa
Zack Smith <zack@puppetlabs.com>
2014-11-24 14:57:31 -0800

    Add Commit

--- manifests/gdm.pp
+++ manifests/gdm.pp
@@ -1,7 +1,10 @@
-# Manage gdm.conf
+# Add entries to /etc/X11/gdm/gdm.conf
 class sox::gdm(
   $fixit = false,
 ) {
+
+  # Puppet will fail if this file does not exist
+  # The fact makes this idempotant
   if $::check_gdm == 'Failed' and $fixit {
     ini_setting { 'gdm.conf AllowRoot=false:':
       path    => '/etc/X11/gdm/gdm.conf',
