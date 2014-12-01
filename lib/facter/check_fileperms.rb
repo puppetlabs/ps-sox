515642f1e1c1236d579be3cf434f017ada75b025
Zack Smith <zack@puppetlabs.com>
2014-12-01 14:03:29 -0800

    Add fileperms

---
+++ lib/facter/check_fileperms.rb
@@ -0,0 +1,30 @@
+Facter.add(:check_fileperms) do
+  confine :kernel => 'Linux'
+  setcode do
+    files = [
+        '/var/log/wtmp',
+        '/var/log/messages',
+        '/var/log/secure',
+        '/var/log/maillog',
+        '/var/log/spooler',
+        '/var/log/boot.log',
+        '/var/log/cron',
+    ]
+
+    checks = {}
+    files.each do |file|
+        next unless File.exist?(file)
+        if File.stat(file).mode == '100600'
+            checks[file] = 'Passed'
+         else
+            checks[file] = 'Failed'
+        end
+    end
+    # Return the first failed check and fail or pass if not found
+    if checks.detect {|k,v| v == 'Failed'}
+        'Failed'
+     else
+        'Passed'
+     end
+  end
+end
