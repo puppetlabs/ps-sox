095b9bf1125179b3d085a49abd975655e40afb2a
Zack Smith <zack@puppetlabs.com>
2014-12-02 10:25:25 -0800

    Fix Syntax error

--- lib/facter/check_su.rb
+++ lib/facter/check_su.rb
@@ -1,7 +1,7 @@
 Facter.add(:check_su) do
   confine :kernel => 'Linux'
   setcode do
-    if File.exist? '/usr/bin/sudo' && File.exist? '/etc/sudoers'
+    if File.exist? '/usr/bin/sudo' and File.exist? '/etc/sudoers'
       'Passed'
     else
       'Failed'
