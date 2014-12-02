595cd4543a95c726acc481ea97e2ccef384af044
Josh Beard <beard@puppetlabs.com>
2014-12-02 14:32:07 -0700

    Add puppet class to enforce ftp compliance
    This commit adds a Puppet class to enforce ftp logging compliance.
    
    This uses Augeas to ensure configuration.
    
    On EL5 and 6, this alters vsftpd via the vsftpd.conf file.
    On EL4, this configures gssftp via the xinetd.d file for the service.
    
    On EL 2 and 7, this configures wu-ftpd via the xinetd.d file for the
    service.

---
+++ manifests/ftplogging.pp
@@ -0,0 +1,42 @@
+class sox::ftplogging (
+  $fixit = true,
+) {
+
+  if $fixit {
+    case $operatingsystemmajrelease {
+      /5|6/: {
+        augeas { 'vsftpd logging':
+          context => '/etc/vsftpd/vsftpd.conf',
+          changes => 'set log_ftp_protocol YES',
+        }
+
+        if !defined(Service['vsftpd']) {
+          service { 'vsftpd':
+            subscribe => Augeas['vsftpd logging'],
+          }
+        }
+      }
+
+      '4': {
+        augeas { 'gssftp logging':
+          context => '/files/etc/xinetd.d/gssftp/service',
+          changes => 'set server_args/value[last() + 1] "-l"',
+          onlyif  => "match server_args/value[. =~ regexp('-l+')] size == 0",
+        }
+      }
+
+      /7|2/: {
+        augeas { 'wu-ftpd logging':
+          context => '/files/etc/xinetd.d/wu-ftpd/service',
+          changes => 'set server_args/value[last() + 1] "-l"',
+          onlyif  => "match server_args/value[. =~ regexp('-l+')] size == 0",
+        }
+      }
+
+      default: {
+        fail("${operatingsystem} ${operatingsystemmajrelease} is not supported")
+      }
+    }
+  }
+
+}
