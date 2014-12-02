2487240df863eb37fb7ebc51b6d0a6808c9c3789
Zack Smith <zack@puppetlabs.com>
2014-12-02 13:09:05 -0800

    Add EL specific logic
    Prior to this commit we did not have the conditional logic from the
    original scripts, this commit adds confines for the legacy code

--- lib/facter/check_account_passwds.rb
+++ lib/facter/check_account_passwds.rb
@@ -2,6 +2,7 @@ require 'etc'
 troubled_accounts = []
 Facter.add(:check_account_passwds) do
   confine :kernel => 'Linux'
+  confine :operatingsystemmajrelease => ['5','6']
   setcode do
     # Loop through /etc/passwd
     Etc.passwd do |user|
@@ -10,6 +11,30 @@ Facter.add(:check_account_passwds) do
         checks[user.name] = 'Passed'
       else
         checks[user.name] = 'Failed'
+      end
+    end
+
+     ## Return 'failed' if any of the packages are not installed
+     if checks.detect {|k,v| v == 'Failed'}
+       'Failed'
+     else
+       'Passed'
+     end
+
+  end
+end
+
+Facter.add(:check_account_passwds) do
+  confine :kernel => 'Linux'
+  confine :operatingsystemmajrelease => ['7','2','3','4']
+  setcode do
+    # Loop through /etc/passwd
+    Etc.passwd do |user|
+      l = `passwd -S #{user.name}`.split
+      if l[1] =~ /.*Empty password.*/
+        checks[user.name] = 'Failed'
+      else
+        checks[user.name] = 'Passed'
       end
     end
 
