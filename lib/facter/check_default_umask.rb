b79cc861312f50101b0af79f41e93914dda34d91
Josh Beard <beard@puppetlabs.com>
2014-12-02 17:13:32 -0700

    Add fact to verify umask
    This commit adds a custom fact to verify 'umask' settings in specified
    files.  Currently, that's '/etc/profile' and '/etc/bashrc'
    
    It's naive.  It simply searches for lines that set a 'umask' that
    doesn't match a specified umask.  If any inconsistent lines are found,
    this fact will report "Failed."
    
    It will not search sourced files from the files it checks.
    
    Reference:
    http://www.stigviewer.com/stig/red_hat_enterprise_linux_5/2014-04-02/finding/V-808
    
    This indicates that umasks should be 077.  This fact uses 022 by
    default.

---
+++ lib/facter/check_default_umask.rb
@@ -0,0 +1,52 @@
+## Description:
+##   This fact scans specified files and searches for lines that set a umask
+##   that differs from our specified umask.  If any such lines are discovered,
+##   this fact will report "Failed", meaning non-compliant.
+##
+## Caveats:
+##   - If a file is sourced by one of the files we're checking, it will not
+##     be checked.
+##   - This does not evaluate conditional logic in the rc files. If umasks are
+##     set based on some conditional logic, this fact will not account for
+##     that.
+##
+## Reference:
+##   http://www.stigviewer.com/stig/red_hat_enterprise_linux_5/2014-04-02/finding/V-808
+##
+## This constant should be set to the desired umask to search for. Any 'umask'
+## line that isn't set to this will cause this fact to report 'Failed'
+##
+UMASK='022'
+
+Facter.add(:check_default_umask) do
+  setcode do
+
+    ## This array contains the absolute path to any files that we want to check
+    ## for umask settings.
+    files = [
+      '/etc/bashrc',
+      '/etc/profile',
+    ]
+
+    result = 'Passed'
+
+    files.each do |file|
+      next unless File.exist?(file)
+      File.open(file,'r') do |rcfile|
+        rcfile.each_line do |line|
+          ## Skip any comments in the file, including comments with leading
+          ## whitespace.
+          next if line =~ /^(\s+)?#.*$/
+
+          ## If there's a 'umask' line that doesn't match our UMASK, fail
+          if line =~ /(umask (?!#{UMASK}).*)/
+            result = "Failed"
+            break
+          end
+        end
+      end
+    end
+
+    result
+  end
+end
