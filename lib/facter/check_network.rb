7d7ed95c4acc132552db39fd0e4a05842a28317d
Zack Smith <zack@puppetlabs.com>
2014-12-03 16:29:53 -0800

    Add check_network

---
+++ lib/facter/check_network.rb
@@ -0,0 +1,40 @@
+require 'augeas'
+Facter.add(:check_network) do
+  confine :kernel => 'Linux'
+  setcode do
+    sysctl_values = {
+      'net.ipv4.tcp_max_syn_backlog'               => '4096',
+      'net.ipv4.tcp_syncookies'                    => '1',
+      'net.ipv4.conf.all.accept_source_route'      => '0',
+      'net.ipv4.conf.all.accept_redirects'         => '0',
+      'net.ipv4.conf.all.secure_redirects'         => '0',
+      'net.ipv4.conf.default.rp_filter'            => '1',
+      'net.ipv4.conf.default.accept_source_route'  => '0',
+      'net.ipv4.conf.default.accept_redirects'     => '0',
+      'net.ipv4.conf.default.secure_redirects'     => '0',
+      'net.ipv4.icmp_echo_ignore_broadcasts'       => '1',
+      'net.ipv4.ip_forward'                        => '0',
+      'net.ipv4.conf.all.send_redirects'           => '0',
+      'net.ipv4.conf.default.send_redirects'       => '0',
+    }
+    checks = {}
+    # Assume failure as file must exist with line to pass
+    Augeas::open do |aug|
+      aug.context = '/files/etc/sysctl.conf'
+      sysctl_values.each do |key,value|
+          # Check the current value against the required value
+          if aug.get(key) == value
+            checks[key] = 'Passed'
+          else
+            checks[key] = 'Failed'
+          end
+      end
+    end
+    # Return 'failed' if any of the keys have incorrect values
+    if checks.detect {|k,v| v == 'Failed'}
+      'Failed'
+    else
+      'Passed'
+    end
+  end
+end
---
+++ manifests/check_network.pp
@@ -0,0 +1,28 @@
+# https://forge.puppetlabs.com/fiddyspence/sysctl
+# As this was the sysctl module choosen over augeasproviders its modeled here
+class sox::check_network(
+    $fixit = false,
+) {
+
+  if $fixit {
+    Sysctl {
+      ensure     => 'present',
+      permanent => 'yes',
+    }
+    sysctl {
+      'net.ipv4.tcp_max_syn_backlog':               value => '4096';
+      'net.ipv4.tcp_syncookies':                    value => '1';
+      'net.ipv4.conf.all.accept_source_route':      value => '0';
+      'net.ipv4.conf.all.accept_redirects':         value => '0';
+      'net.ipv4.conf.all.secure_redirects':         value => '0';
+      'net.ipv4.conf.default.rp_filter':            value => '1';
+      'net.ipv4.conf.default.accept_source_route':  value => '0';
+      'net.ipv4.conf.default.accept_redirects':     value => '0';
+      'net.ipv4.conf.default.secure_redirects':     value => '0';
+      'net.ipv4.icmp_echo_ignore_broadcasts':       value => '1';
+      'net.ipv4.ip_forward':                        value => '0';
+      'net.ipv4.conf.all.send_redirects':           value => '0';
+      'net.ipv4.conf.default.send_redirects':       value => '0';
+    }
+  }
+}
