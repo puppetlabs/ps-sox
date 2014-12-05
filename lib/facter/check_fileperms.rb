06e03a19123aaf5e40836324c4ac22387c006a93
Josh Beard <beard@puppetlabs.com>
2014-12-05 11:48:40 -0700

    Add fix_fileperms fact; adjust indent
    This commit adds a 'fix_fileperms' fact, which lists the files that have
    non-compliant file permissions (not 0600) as a comma-separated list.
    
    This also adjusts indentation for style.
    
    Functional changes:
      - 'Failed' files get added to a 'failed_files' array
      - Create 'fix_fileperms' fact, which returns the 'failed_files' array
      as a comma-separated list

--- lib/facter/check_fileperms.rb
+++ lib/facter/check_fileperms.rb
@@ -1,30 +1,39 @@
+failed_files = []
 Facter.add(:check_fileperms) do
   confine :kernel => 'Linux'
   setcode do
     files = [
-        '/var/log/wtmp',
-        '/var/log/messages',
-        '/var/log/secure',
-        '/var/log/maillog',
-        '/var/log/spooler',
-        '/var/log/boot.log',
-        '/var/log/cron',
+      '/var/log/wtmp',
+      '/var/log/messages',
+      '/var/log/secure',
+      '/var/log/maillog',
+      '/var/log/spooler',
+      '/var/log/boot.log',
+      '/var/log/cron',
     ]
 
     checks = {}
     files.each do |file|
-        next unless File.exist?(file)
-        if sprintf("%o", File.stat(file).mode) == '100600'
-            checks[file] = 'Passed'
-         else
-            checks[file] = 'Failed'
-        end
+      next unless File.exist?(file)
+      if sprintf("%o", File.stat(file).mode) == '100600'
+        checks[file] = 'Passed'
+      else
+        checks[file] = 'Failed'
+        failed_files << file
+      end
     end
     # Return the first failed check and fail or pass if not found
     if checks.detect {|k,v| v == 'Failed'}
-        'Failed'
-     else
-        'Passed'
-     end
+      'Failed'
+    else
+      'Passed'
+    end
+  end
+end
+
+Facter.add(:fix_fileperms) do
+  confine :kernel => 'Linux'
+  setcode do
+    failed_files.join(',')
   end
 end
